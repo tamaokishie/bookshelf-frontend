baef47ea3601bd6f7161d1c81d7b595271d2365f
ShieTamaoki <shie0041@gmail.com>
2025-05-21 02:41:17 +0900

    fix books

--- src/data/books.js
+++ src/data/books.js
@@ -1,43 +1,46 @@
+
+const basePath = process.env.PUBLIC_URL;
+
 const books = [
   {
     id: 1,
     title: 'Web APIの設計',
-    image: '/images/image_webapi.jpg',
+    image: `${basePath}/images/image_webapi.jpg`,
     totalPages: 300,
     readPages: 150,
   },
   {
     id: 2,
     title: 'ドメイン駆動設計入門',
-    image: '/images/image_domainstart.jpg',
+    image: `${basePath}/images/image_domainstart.jpg`,
     totalPages: 250,
     readPages: 100,
   },
   {
     id: 3,
     title: '「入門」ドメイン駆動設計',
-    image: '/images/image_domain2.jpg',
+    image: `${basePath}/images/image_domain2.jpg`,
     totalPages: 250,
     readPages: 100,
   },
   {
     id: 4,
     title: 'モデルベース要件定義テクニック',
-    image: '/images/image_sdem.jpg',
+    image: `${basePath}/images/image_sdem.jpg`,
     totalPages: 250,
     readPages: 100,
   },
   {
     id: 5,
     title: 'プログラマー脳',
-    image: '/images/image_programer_brain.jpg',
+    image: `${basePath}/images/image_programer_brain.jpg`,
     totalPages: 250,
     readPages: 100,
   },
   {
     id: 6,
     title: 'なぜオブジェクト指向で作るのか',
-    image: '/images/image_object.jpg',
+    image: `${basePath}/images/image_object.jpg`,
     totalPages: 250,
     readPages: 100,
   },
